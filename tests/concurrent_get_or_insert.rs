940c61b8563fc035f77672247de96ea210aa1109
Ugur Arikan (DHL Data & Analytics) <ugur.arikan@dpdhl.com>
2024-08-09 11:10:21 +0200

    get_or_insert and get_or_insert_with methods
    These methods are partially thread safe due to the returned mutable reference.

---
+++ tests/concurrent_get_or_insert.rs
@@ -0,0 +1,88 @@
+#![cfg(not(miri))]
+
+use orx_concurrent_option::*;
+use std::time::Duration;
+use test_case::test_matrix;
+
+#[test_matrix(
+    [2, 4, 8, 16],
+    [false, true]
+)]
+fn concurrent_get_or_insert_single_writer(num_readers: usize, do_sleep: bool) {
+    let maybe = ConcurrentOption::some(7.to_string());
+    let maybe_ref = &maybe;
+
+    std::thread::scope(|s| {
+        for _ in 0..(num_readers / 2) {
+            s.spawn(move || reader(do_sleep, maybe_ref));
+        }
+
+        s.spawn(move || get_or_inserter(do_sleep, maybe_ref));
+
+        for _ in 0..(num_readers / 2) {
+            s.spawn(move || reader(do_sleep, maybe_ref));
+        }
+    });
+}
+
+#[test_matrix(
+    [4, 8],
+    [2, 4, 8, 16],
+    [false, true]
+)]
+fn concurrent_get_or_insert_multiple_writer(
+    num_writers: usize,
+    num_readers: usize,
+    do_sleep: bool,
+) {
+    let maybe = ConcurrentOption::some(9.to_string());
+    let maybe_ref = &maybe;
+
+    std::thread::scope(|s| {
+        for _ in 0..(num_writers / 2) {
+            s.spawn(move || get_or_inserter(do_sleep, maybe_ref));
+        }
+
+        for _ in 0..num_readers {
+            s.spawn(move || reader(do_sleep, maybe_ref));
+        }
+
+        for _ in 0..(num_writers / 2) {
+            s.spawn(move || get_or_inserter(do_sleep, maybe_ref));
+        }
+    });
+}
+
+// helpers
+fn reader(do_sleep: bool, maybe: &ConcurrentOption<String>) {
+    for _ in 0..100 {
+        sleep(do_sleep);
+
+        let is_nine_or_seven = maybe
+            .map_ref(|x| x == &7.to_string() || x == &9.to_string())
+            .unwrap_or(false);
+        assert!(is_nine_or_seven);
+    }
+}
+
+fn get_or_inserter(do_sleep: bool, maybe: &ConcurrentOption<String>) {
+    for i in 0..100 {
+        sleep(do_sleep);
+        match i % 5 {
+            1 => {
+                let _ = unsafe { maybe.get_or_insert(7.to_string()) };
+            }
+            3 => {
+                let _ = unsafe { maybe.get_or_insert(9.to_string()) };
+            }
+            _ => {}
+        }
+    }
+}
+
+fn sleep(do_sleep: bool) {
+    if do_sleep {
+        let duration = Duration::from_millis(24);
+        std::thread::sleep(duration);
+    }
+}
